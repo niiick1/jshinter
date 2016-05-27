ba92b5a920f32902d608f3692b06b988c806b574
nick1 <nicolas.cnunes@gmail.com>
2016-05-27 04:50:40 -0300

    Include bitwise rule
    This rule warns about bitwise operators/assignments.

--- jshinter/analyzer/JSHinterListener.java
+++ jshinter/analyzer/JSHinterListener.java
@@ -1,8 +1,16 @@
 package jshinter.analyzer;
 
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.TokenStream;
+
 import jshinter.antlr.ECMAScriptBaseListener;
 import jshinter.antlr.ECMAScriptParser;
-import jshinter.antlr.ECMAScriptParser.ProgramContext;
+import jshinter.antlr.ECMAScriptParser.AssignmentOperatorContext;
+import jshinter.antlr.ECMAScriptParser.BitAndExpressionContext;
+import jshinter.antlr.ECMAScriptParser.BitNotExpressionContext;
+import jshinter.antlr.ECMAScriptParser.BitOrExpressionContext;
+import jshinter.antlr.ECMAScriptParser.BitShiftExpressionContext;
+import jshinter.antlr.ECMAScriptParser.BitXOrExpressionContext;
 
 public class JSHinterListener extends ECMAScriptBaseListener {
 
@@ -19,10 +27,53 @@ public class JSHinterListener extends ECMAScriptBaseListener {
 		this.parser = parser;
 		this.DEBUG = debugMode;
 	}
+	
+	private void reportError(String msg, Token t) {
+		System.out.printf("%d,%d: %s.\n", t.getLine(), t.getCharPositionInLine() + 1, msg);
+	}
 
 	@Override
-	public void enterProgram(ProgramContext ctx) {
-//		super.enterProgram(ctx);
-		System.out.println("PROGRAMA JAVASCRIPT");
+	public void enterBitXOrExpression(BitXOrExpressionContext ctx) {
+		TokenStream ts = parser.getTokenStream();
+
+		reportError(String.format("Unexpected use of '%s'", ctx.getChild(1).getText()), ts.get(ctx.getChild(1).getSourceInterval().a));
+	}
+
+	@Override
+	public void enterBitShiftExpression(BitShiftExpressionContext ctx) {
+		TokenStream ts = parser.getTokenStream();
+
+		reportError(String.format("Unexpected use of '%s'", ctx.getChild(1).getText()), ts.get(ctx.getChild(1).getSourceInterval().a));
+	}
+
+	@Override
+	public void enterBitNotExpression(BitNotExpressionContext ctx) {
+		TokenStream ts = parser.getTokenStream();
+
+		reportError(String.format("Unexpected use of '%s'", ctx.getChild(1).getText()), ts.get(ctx.getChild(1).getSourceInterval().a));
+	}
+
+	@Override
+	public void enterBitAndExpression(BitAndExpressionContext ctx) {
+		TokenStream ts = parser.getTokenStream();
+
+		reportError(String.format("Unexpected use of '%s'", ctx.getChild(1).getText()), ts.get(ctx.getChild(1).getSourceInterval().a));
+	}
+
+	@Override
+	public void enterBitOrExpression(BitOrExpressionContext ctx) {
+		TokenStream ts = parser.getTokenStream();
+
+		reportError(String.format("Unexpected use of '%s'", ctx.getChild(1).getText()), ts.get(ctx.getChild(1).getSourceInterval().a));
+	}
+
+	@Override
+	public void enterAssignmentOperator(AssignmentOperatorContext ctx) {
+		Token t = ctx.getStart();
+		int type = t.getType();
+		
+		if (type >= 45 || type <= 50) {
+			reportError(String.format("Unexpected use of '%s'", t.getText()), t);
+		}
 	}
 }
